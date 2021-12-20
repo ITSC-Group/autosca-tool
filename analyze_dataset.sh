eac0eacf7ce6c6168ada1a1b114cb02a27ea81a8
Jan Drees <jan.drees@uni-wuppertal.de>
2021-12-20 22:49:31 +0100

    Split script into stage 1 and 2-4

---
+++ analyze_dataset.sh
@@ -0,0 +1,73 @@
+#! /bin/bash
+PARALLEL_THREADS=$(grep -c ^processor /proc/cpuinfo)
+CROSSVALIDATION_TECHNIQUE="kccv"
+CROSSVALIDATION_ITERATIONS=30
+HYPERPARAMETER_ITERATIONS=10
+
+set -e
+
+usage()
+{
+    echo 'usage example: ./analyze_dataset.sh --folder /home/datasets/2021-12-20-apollolvdamnvulnerableopenssl-server'
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        --folder )              shift
+                                FOLDER=$1
+                                ;;
+        --threads )             shift
+                                PARALLEL_THREADS=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     echo "Unknown argument $1"
+                                usage
+                                exit 1
+    esac
+    shift
+done
+
+echo "Analyzing dataset folder $FOLDER"
+cd "$FOLDER" || exit
+
+CONFIG="$FOLDER/config.md"
+
+echo "Starting feature extraction"
+echo " " >> "$CONFIG"
+echo "# Feature Extraction" >> "$CONFIG"
+START_TIME=$(date +%s)
+pipenv run python3 feature_extraction/extract.py --folder="$FOLDER" 2>&1 | tee "$FOLDER/Feature Extraction.log"
+END_TIME=$(date +%s)
+DURATION="$(($END_TIME-$START_TIME))"
+echo "Finished feature extraction, execution took $DURATION seconds"
+echo "## Execution Time" >> "$CONFIG"
+echo "$DURATION seconds" >> "$CONFIG"
+
+echo "Starting $CROSSVALIDATION_TECHNIQUE classification model training"
+echo " " >> "$CONFIG"
+echo "# Machine Learning" >> "$CONFIG"
+echo "## Parameters" >> "$CONFIG"
+echo "Using $CROSSVALIDATION_TECHNIQUE crossvalidation technique" >> "$CONFIG"
+echo "Doing $CROSSVALIDATION_ITERATIONS crossvalidation iterations" >> "$CONFIG"
+echo "Doing $HYPERPARAMETER_ITERATIONS hyperparameter optimization iterations" >> "$CONFIG"
+
+START_TIME=$(date +%s)
+pipenv run python3 classification_model/train_models.py --folder="$FOLDER" --cv_technique=$CROSSVALIDATION_TECHNIQUE --cv_iterations=$CROSSVALIDATION_ITERATIONS --iterations=$HYPERPARAMETER_ITERATIONS --n_jobs=$PARALLEL_THREADS 2>&1 | tee "$FOLDER/Classification Model Training $1.log"
+END_TIME=$(date +%s)
+DURATION="$(($END_TIME-$START_TIME))"
+echo "Finished $CROSSVALIDATION_TECHNIQUE classification model training, execution took $DURATION seconds"
+echo "## Execution Time" >> "$CONFIG"
+echo "$DURATION seconds" >> "$CONFIG"
+
+echo "Generating report"
+pipenv run python3 classification_model/pvalues_calculation.py --folder="$FOLDER" --cv_technique=$CROSSVALIDATION_TECHNIQUE 2>&1 | tee "$FOLDER/Report Generation $1.log"
+
+echo "Plotting the machine learning results"
+pipenv run python3 classification_model/plot_results.py --folder="$FOLDER" --cv_technique=$CROSSVALIDATION_TECHNIQUE --cv_iterations=$CROSSVALIDATION_ITERATIONS 2>&1 | tee "$FOLDER/Classification Model Plotting $1.log"
+echo "Finished plotting"
+echo " " >> "$CONFIG"
+
+echo "Experiment run finished" >> "$CONFIG"
+echo "Experiment run finished"
--- generate_docker_dataset.sh
+++ generate_docker_dataset.sh
@@ -4,29 +4,25 @@ SUT_HOST="localhost"
 CAPTURE_HOST="localhost"
 SUT_PORT=443
 SUT_NAME="apollolv/damnvulnerableopenssl-server"
-TAG=""
+SUT_INTERFACE="docker0"
 USE_TLS_ATTACKER=1
-PARALLEL_THREADS=$(grep -c ^processor /proc/cpuinfo)
-CROSSVALIDATION_TECHNIQUE="kccv"
-CROSSVALIDATION_ITERATIONS=30
-HYPERPARAMETER_ITERATIONS=10
 CLIENT_ARGUMENTS=""
 SERVER_ARGUMENTS=""
 DOCKER_ARGUMENTS=""
-DATASET_FOLDER=""
+FOLDER="/home/datasets/$(date --iso-8601)-unnamed"
 
 set -e
 
 usage()
 {
-    echo 'usage example: ./start.sh --tlsattacker --tag googletest --host www.google.com --interface enp3s0 --clientarguments "--repetitions 20 --noskip"'
+    echo 'usage example: ./start.sh --tlsattacker --container apollolv/damnvulnerableopenssl-server --folder /home/datasets/2021-12-20-apollolvdamnvulnerableopenssl-server --clientarguments "--repetitions 200 --noskip"'
 }
 
 ALL_PARAMETERS=$*
 
 while [ "$1" != "" ]; do
     case $1 in
-        --name )                shift
+        --container )           shift
                                 SUT_NAME="$1"
                                 ;;
         --tlsattacker )         USE_TLS_ATTACKER=1
@@ -34,11 +30,8 @@ while [ "$1" != "" ]; do
         --port )                shift
                                 SUT_PORT=$1
                                 ;;
-        --tag )                 shift
-                                TAG=$1
-                                ;;
-        --datasetfolder )       shift
-                                DATASET_FOLDER=$1
+        --folder )              shift
+                                FOLDER=$1
                                 ;;
         --clientarguments )     shift
                                 CLIENT_ARGUMENTS=$1
@@ -49,9 +42,6 @@ while [ "$1" != "" ]; do
         --dockerarguments )     shift
                                 DOCKER_ARGUMENTS=$1
                                 ;;
-        --threads )             shift
-                                PARALLEL_THREADS=$1
-                                ;;
         -h | --help )           usage
                                 exit
                                 ;;
@@ -61,18 +51,6 @@ while [ "$1" != "" ]; do
     esac
     shift
 done
-
-# shellcheck disable=SC2086
-if [ "$TAG" ]; then
-    SANITIZED_SUT_NAME=$(echo $TAG | tr -dc '[:alnum:]._-')
-else
-    SANITIZED_SUT_NAME=$(echo $SUT_NAME | tr -dc '[:alnum:]._-')
-fi
-if [ "$DATASET_FOLDER" ]; then
-    FOLDER="$DATASET_FOLDER/$(date --iso-8601)-$SANITIZED_SUT_NAME"
-else
-    FOLDER="$TOOL_FOLDER/datasets/$(date --iso-8601)-$SANITIZED_SUT_NAME"
-fi
 
 echo "Creating dataset folder $FOLDER"
 mkdir -p "$FOLDER"
@@ -125,7 +103,6 @@ if [ "$USE_TLS_ATTACKER" = "0" ]; then
     echo "host=$SUT_HOST" >> "$TOOL_FOLDER/tls_test_tool_client/config/$SANITIZED_SUT_NAME.conf"
 fi
 
-SUT_INTERFACE="docker0"
 echo "Starting packet capture on the default docker interface $SUT_INTERFACE"
 
 echo "Special privileges are needed to capture network traffic and add delays to interfaces"
@@ -171,40 +148,5 @@ echo "Stopping system under test docker container"
 docker stop "$SANITIZED_SUT_NAME"
 # kill -2 $DOCKERLOG_PID
 
-echo "Starting feature extraction"
-echo " " >> "$CONFIG"
-echo "# Feature Extraction" >> "$CONFIG"
-START_TIME=$(date +%s)
-pipenv run python3 feature_extraction/extract.py --folder="$FOLDER" 2>&1 | tee "$FOLDER/Feature Extraction.log"
-END_TIME=$(date +%s)
-DURATION="$(($END_TIME-$START_TIME))"
-echo "Finished feature extraction, execution took $DURATION seconds"
-echo "## Execution Time" >> "$CONFIG"
-echo "$DURATION seconds" >> "$CONFIG"
-
-echo "Starting $CROSSVALIDATION_TECHNIQUE classification model training"
-echo " " >> "$CONFIG"
-echo "# Machine Learning" >> "$CONFIG"
-echo "## Parameters" >> "$CONFIG"
-echo "Using $CROSSVALIDATION_TECHNIQUE crossvalidation technique" >> "$CONFIG"
-echo "Doing $CROSSVALIDATION_ITERATIONS crossvalidation iterations" >> "$CONFIG"
-echo "Doing $HYPERPARAMETER_ITERATIONS hyperparameter optimization iterations" >> "$CONFIG"
-
-START_TIME=$(date +%s)
-pipenv run python3 classification_model/train_models.py --folder="$FOLDER" --cv_technique=$CROSSVALIDATION_TECHNIQUE --cv_iterations=$CROSSVALIDATION_ITERATIONS --iterations=$HYPERPARAMETER_ITERATIONS --n_jobs=$PARALLEL_THREADS 2>&1 | tee "$FOLDER/Classification Model Training $1.log"
-END_TIME=$(date +%s)
-DURATION="$(($END_TIME-$START_TIME))"
-echo "Finished $CROSSVALIDATION_TECHNIQUE classification model training, execution took $DURATION seconds"
-echo "## Execution Time" >> "$CONFIG"
-echo "$DURATION seconds" >> "$CONFIG"
-
-echo "Generating report"
-pipenv run python3 classification_model/pvalues_calculation.py --folder="$FOLDER" --cv_technique=$CROSSVALIDATION_TECHNIQUE 2>&1 | tee "$FOLDER/Report Generation $1.log"
-
-echo "Plotting the machine learning results"
-pipenv run python3 classification_model/plot_results.py --folder="$FOLDER" --cv_technique=$CROSSVALIDATION_TECHNIQUE --cv_iterations=$CROSSVALIDATION_ITERATIONS 2>&1 | tee "$FOLDER/Classification Model Plotting $1.log"
-echo "Finished plotting"
-echo " " >> "$CONFIG"
-
-echo "Experiment run finished" >> "$CONFIG"
-echo "Experiment run finished"
+echo "Dataset generation finished" >> "$CONFIG"
+echo "Dataset generation finished"
